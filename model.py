50a0b4c22f32cb4b10349fe2cc92482a9908cb55
Huw Day <85741581+HuwWDay@users.noreply.github.com>
2026-07-28 16:16:18 +0100

    Step 20: r_squared

--- model.py
+++ model.py
@@ -228,8 +228,15 @@ def root_mean_squared_error(y_true, y_pred):
     # TODO: Return the root mean squared error between y_true and y_pred.
     return np.sqrt(np.mean((y_true-y_pred)**2))
 
-# Step 20 - r_squared (not yet solved)
-# TODO: implement
+# Step 20 - r_squared
+def r_squared(y_true, y_pred):
+    # TODO: Compute the coefficient of determination R^2.
+    res_sum = np.sum((y_true-y_pred)**2)
+    y_hat = np.mean(y_true)
+    square_sum = np.sum((y_true-y_hat)**2)
+    if square_sum == 0.0:
+        return np.nan
+    return 1.0 - res_sum/square_sum
 
 # Step 21 - evaluate_regression (not yet solved)
 # TODO: implement
