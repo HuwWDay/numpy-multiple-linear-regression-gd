3b1497c047d43b5060e4a78f36388d95d0dfc211
Huw Day <85741581+HuwWDay@users.noreply.github.com>
2026-07-28 16:26:01 +0100

    Step 23: weights_l2_distance

--- model.py
+++ model.py
@@ -259,8 +259,10 @@ def learning_curve_data(train_losses, val_losses):
     
     return epochs, train_list, val_list
 
-# Step 23 - weights_l2_distance (not yet solved)
-# TODO: implement
+# Step 23 - weights_l2_distance
+def weights_l2_distance(w_gd, w_closed):
+    # TODO: Compute the L2 distance between two weight vectors
+    return np.sqrt(np.sum((w_gd-w_closed)**2))
 
 # Step 24 - create_lr_model (not yet solved)
 # TODO: implement
